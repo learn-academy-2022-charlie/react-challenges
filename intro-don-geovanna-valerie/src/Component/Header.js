7070147675de2d41250d305f3aa9aa9bc261a1e2
vjtovar <vjtovar25@gmail.com>
2022-05-23 11:13:17 -0700

    header complete

---
+++ intro-don-geovanna-valerie/src/Component/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Header extends Component {
+  render(){
+    return(
+      <>
+        <h1>Group Profile</h1>
+      </>
+    )
+  }
+}
+export default Header
