0a4424d001de2266d3571ba84056d9b3208ef7ee
Geovannapg12 <geo.p2334@gmail.com>
2022-05-23 11:34:46 -0700

    added footer and profile

---
+++ intro-don-geovanna-valerie/src/Component/Profile.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Profile extends Component {
+  render(){
+    return(
+      <>
+        <p> Don is from Chicago</p>
+        <p>Valerie is from El Paso Texas</p>
+        <p>Geovanna is from Mexico<p>
+      </>
+    )
+  }
+}
+export default Profile
