1e167ba93eb8db3481e49674c86d9d17ae22fa09
jennerdulce <jennerdulce@gmail.com>
2022-05-23 16:40:22 -0700

    finished challenge

---
+++ state-denise-jenner/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React, { Component } from 'react';
+import Square from './components/Square'
+
+class App extends Component {
+  render() {
+    return(
+      <>
+        <Square />
+        <Square />
+      </>
+    )
+  }
+}
+
+export default App
