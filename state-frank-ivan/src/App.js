5b73bea9bb5d8f03df36a1c00f360cc66af57617
F-Rivers <francisco.rivera92@icloud.com>
2022-05-23 16:34:15 -0700

    good luck amigo

---
+++ state-frank-ivan/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Box from './Components/Box'
+
+
+class App extends Component{
+  render(){
+    return(
+      <>
+        <Box />
+      </>
+    )
+  }
+}
+export default App
---
+++ state-frank-ivan/src/Components/Box.js
@@ -0,0 +1,12 @@
+import React, {component} from 'react'
+
+class Box extends component {
+    render () {
+        return(
+        <> <h1>Color Box</h1>
+        </>
+    )
+}
+}
+
+export default Box
