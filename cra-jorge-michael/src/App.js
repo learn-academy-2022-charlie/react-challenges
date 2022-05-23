78332abd9e16a65f5b160a35db3361eae377a675
LoneWanderer101 <sgtlinares@gmail.com>
2022-05-23 11:19:24 -0700

    started react app

---
+++ cra-jorge-michael/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Header from './component/Header'
+import Profile from './component/Profile'
+class App extends Component{
+  render(){
+    return(
+      <>
+      <Header />
+      <Profile />
+      </>
+    )
+  }
+}
+
+export default App
---
+++ cra-jorge-michael/src/component/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Profile extends Component{
+  render(){
+    return(
+      <>
+        <h2>Michael and Jorge are veterans who are passionate about coding and eating</h2>
+      </>
+    )
+  }
+}
+
+export default Profile
