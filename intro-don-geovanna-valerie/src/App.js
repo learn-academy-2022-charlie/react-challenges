3629a73d67bfb82524d4126916105c4c01d1a73f
Don Yoon <dongho.yoon@gmail.com>
2022-05-23 10:56:12 -0700

    created app, changing driver for other components

---
+++ intro-don-geovanna-valerie/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Profiler } from 'react'
+
+class App extends Component {
+  render(){
+    return(
+      <>
+      <Header/>
+      <Profile/>
+      <Footer/>
+      <Favorites/>
+      </>
+    )
+  }
+}
+export default App
