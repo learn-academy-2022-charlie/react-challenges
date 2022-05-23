d3bc5ac89b98402170f4eaa11f2718438d157d26
Denise-Carlson <carlsonden@gmail.com>
2022-05-23 12:02:39 -0700

    Completed up to Footer challenge

---
+++ intro-denise-jenner/src/components/Header.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+
+class Header extends Component {
+    render(){
+        return(
+            <>
+            <h1>Our First React App</h1>
+            </>
+        )
+    }
+}
+export default Header
---
+++ intro-denise-jenner/src/components/Profile.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react'
+
+
+class Profile extends Component {
+    render(){
+        return(
+            <>
+            <h2>Developers Profile</h2>
+            <h3>Denise Carlson</h3>
+            <p>
+                New React Developer making first application. Lives in Houston, TX
+                Enjoys walking and hiking.
+            </p>
+            <h3>Jenner Dulce</h3>
+            <p>
+            New React Developer making first application. Lives in San Diego, CA
+                Enjoys video games and surfing.
+            </p>
+            </>
+        )
+    }
+}
+export default Profile
