d62d92375f7189dc646275af5a3ce4658fc2b4f8
ios10 <trikshotzz05@gmail.com>
2022-05-23 11:28:25 -0700

    first 3

---
+++ intro-ivan-frank/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Footer extends Component{
+	render(){
+		return(
+			<>
+			<h3> Charlie Cohort 2022</h3>
+
+			</>
+
+		)
+	}
+}
+export default Footer
---
+++ intro-ivan-frank/src/components/Profile.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Profile extends Component{
+	render(){
+		return(
+			<>
+			Ivan- likes to make music
+			<br/>
+			Frank- likes to sleep
+			</>
+
+
+		)
+	}
+}
+export default Profile
