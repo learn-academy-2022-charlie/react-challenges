df8fe7a81df107f38ff984059bb634484b7d4af0
Don Yoon <dongho.yoon@gmail.com>
2022-05-24 14:55:04 -0700

    minor changes in formatting

--- state-valerie-geovanna-don/src/App.js
+++ state-valerie-geovanna-don/src/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react'
 // import Button from './components/Button'
 import AddRemove from './components/AddRemove'
+import './App.css'
+
 
 class App extends Component{
  
   render() {
     return(
     <>
-    
-    <AddRemove/>
+        <AddRemove/>
     </>
     )
   }
