8b9778e3e991ba53422a921074adbd44bfc31c0a
jennerdulce <jennerdulce@gmail.com>
2022-05-26 16:44:55 -0700

    finished

--- inputs-valerie-jenner/src/App.js
+++ inputs-valerie-jenner/src/App.js
@@ -14,6 +14,18 @@ class App extends Component {
   }
 
   blah = (word) => {
+    // create variable named blahString to hold a string
+    // Use for loop to iterate through word
+    // use conditonals to decide what letter should be added
+    // Conditional if blahString === "" 
+    // blahString = blahString + B 
+    // Conditional if blahString i-1 === B   
+    // blahString = blahString + L
+    // Conditional if blahString i-1 === L   
+    // blahString = blahString + A
+    // Conditional if blahString i-1 === A  
+    // blahString = blahString + B
+
     let blahString = ""
     for(let i=0; i<word.length; i++){
         if(blahString === ""){
@@ -25,24 +37,8 @@ class App extends Component {
         } else if(blahString[i-1] === "A"){
             blahString = blahString + "B"
         }
-
-        
     }
     return `I hear you saying  ${blahString}  . Is that correct?`
-
-    // create variable named blahString to hold a string
-    // Use for loop to iterate through word
-    // use conditonals to decide what letter should be added
-    // Conditional if blahString === "" 
-    // blahString = blahString + B 
-    // Conditional if blahString i-1 === B   
-    // blahString = blahString + L
-    //Conditional if blahString i-1 === L   
-    // blahString = blahString + A
-    //Conditional if blahString i-1 === A  
-    // blahString = blahString + B
-
-
 }
 
   render() {
@@ -52,28 +48,25 @@ class App extends Component {
         <h1>Greeter App</h1>
         <p>Say Something</p>
         <input 
-          type="text" 
-          
+          type="text"  
           onChange={this.handleChange}
         />
+
         <Robot
         name="Good Robot"
+        firstName="Jenner"
         word={'I hear you saying ' + this.state.word + '. Is that correct?'}
         />
+
         <Robot
         name="Bad Robot"
         word={this.blah(this.state.word)}
         />
+
         <Robot
         name="Kanyebot 5000"
         word={'I\'m gonna let you finish but Beyonce is ' + this.state.word}
         />
-
-
-
-
-    
-        
       </>
     )
   }
