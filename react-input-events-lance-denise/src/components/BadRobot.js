2ec950eb0dcc188b5b5300d5400591aab49d606c
Denise-Carlson <carlsonden@gmail.com>
2022-05-26 16:53:23 -0700

    started doing bad robot function

--- react-input-events-lance-denise/src/components/BadRobot.js
+++ react-input-events-lance-denise/src/components/BadRobot.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 
  class BadRobot extends Component {
+  evilIt = (userInput) => {
+    console.log("arraying it", userInput.split(" "))
+    //if the userInput string is not empty, enact the variable called evil.
+    if(userInput !== "") {
+        let evil = userInput.split(" ").map(value => {
+        
+            return value = " Bla"
+
+        })
+        return beep
+    }
+
+
+  
+}
+
   render() {
     return (
-      <div>BadRobot</div>
+      <>
+        <h3>Bad Robot</h3>
+        <h4>The Mean Version</h4>
+        <p>{this.beepIt(this.props.robotString)}</p>
+    </>
+
     )
   }
 }
