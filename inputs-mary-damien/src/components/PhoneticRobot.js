a83988c2fd1482e26ca80d18893a28d6374cb291
Damien Richcreek <damien@richcreek.dev>
2022-05-27 04:41:18 -0700

    added remaining phonetic alphabet

--- inputs-mary-damien/src/components/PhoneticRobot.js
+++ inputs-mary-damien/src/components/PhoneticRobot.js
@@ -6,22 +6,87 @@ class PhoneticRobot extends Component {
     let newStr = ""
     let inputText = this.props.phoneText
       for(let i = 0; i < inputText.length; i++ ) {
-          switch(inputText[i]){
+          switch(inputText[i].toLowerCase()){
             default:
               break
             case "a":
-              newStr += 'Alpha '
+              newStr += "Alpha "
               break
             case "b":
-              newStr += 'Bravo '
+              newStr += "Bravo "
               break
             case "c":
-              newStr += 'Charlie '
+              newStr += "Charlie "
               break
             case "d":
-              newStr += 'Delta '
+              newStr += "Delta "
               break
-            
+            case "e":
+              newStr += "Echo "
+              break
+            case "f":
+              newStr += "Foxtrot "
+              break
+            case "g":
+              newStr += "Golf "
+              break
+            case "h":
+              newStr += "Hotel "
+              break
+            case "i":
+              newStr += "India "
+              break
+            case "j":
+              newStr += "Juliet "
+              break
+            case "k":
+              newStr += "Kilo "
+              break
+            case "l":
+              newStr += "Lima "
+              break
+            case "m":
+              newStr += "Mike "
+              break
+            case "n":
+              newStr += "November "
+              break
+            case "o":
+              newStr += "Oscar "
+              break
+            case "p":
+              newStr += "Papa "
+              break
+            case "q":
+              newStr += "Quebec "
+              break
+            case "r":
+              newStr += "Romeo "
+              break
+            case "s":
+              newStr += "Sierra "
+              break
+            case "t":
+              newStr += "Tango "
+              break
+            case "u":
+              newStr += "Uniform "
+              break
+            case "v":
+              newStr += "Victor "
+              break
+            case "w":
+              newStr += "Whiskey "
+              break
+            case "x":
+              newStr += "X-Ray "
+              break
+            case "y":
+              newStr += "Yankee "
+              break
+            case "z":
+              newStr += "Zulu"
+              break
           }
       }
       return newStr
